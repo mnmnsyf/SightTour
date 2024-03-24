d9bdcd3541bb8ab95bfe8380e3df114216d73b47
sheyongfei <sheyongfei@digisky.com>
2024-03-24 18:34:25 +0800

    +添加状态树有关插件

--- Source/SightTour/SightTour.Build.cs
+++ Source/SightTour/SightTour.Build.cs
@@ -4,16 +4,43 @@ using UnrealBuildTool;
 
 public class SightTour : ModuleRules
 {
-	public SightTour(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public SightTour(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicIncludePaths.AddRange(
             new string[] {
-                "SightTour"
+                "SightTour",
+                "SightTour/Pickup",
+                "SightTour/StateTrees",
             }
         );
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
-	}
+        PublicDependencyModuleNames.AddRange(
+            new string[] {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "InputCore",
+                "HeadMountedDisplay",
+                "EnhancedInput",
+                "NavigationSystem",
+                "DeveloperSettings",
+                "AIModule",
+                "StateTreeModule",
+                "GameplayStateTreeModule",
+                "GameplayTasks",
+                "GameplayTags",
+               // "StructUtilsEngine",
+            });
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[] {
+                "Slate",
+                "SlateCore",
+                "PropertyPath",
+                "StructUtils",
+            }
+        );
+    }
 }
