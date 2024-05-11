7919e1adaf70e8b3e716d408b200a782425d1256
Syf <2469073455@qq.com>
2024-05-11 23:30:39 +0800

    +添加加载关卡的接口

--- Source/SightTour/System/SightTourLevelManager.cpp
+++ Source/SightTour/System/SightTourLevelManager.cpp
@@ -2,4 +2,34 @@
 
 
 #include "System/SightTourLevelManager.h"
+#include "SightTourGameInstance.h"
+#include "Camera/PlayerCameraManager.h"
+#include "Kismet/GameplayStatics.h"
 
+USightTourLevelManager* USightTourLevelManager::Instance = nullptr;
+
+void USightTourLevelManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Instance = this;
+	Super::Initialize(Collection);
+}
+
+bool USightTourLevelManager::LoadNewLevel(FName SceneName)
+{
+	//TODO:start acting
+
+	if (SceneName == OldSceneName)
+	{
+		//TODO:end acting
+		return false;
+	}
+
+	APlayerCameraManager* CameraManager = UGameplayStatics::GetPlayerCameraManager(this, 0);
+	check(CameraManager);
+
+	CameraManager->StartCameraFade(0.f, 1.f, 0.f, FLinearColor::Black, true, true);
+
+	//TODO:widget visible
+
+
+}
--- Source/SightTour/System/SightTourLevelManager.h
+++ Source/SightTour/System/SightTourLevelManager.h
@@ -13,5 +13,32 @@ UCLASS()
 class SIGHTTOUR_API USightTourLevelManager : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+
+public:
+	//-------------------------内部流程接口，外部人员不需关心 start------------------------------------
+
+	USightTourLevelManager() { }
+
+	void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	static USightTourLevelManager* Get() { return Instance; }
+
+	//-------------------------内部流程接口，外部人员不需关心 end------------------------------------
+
+public:
+	//-------------------------关卡流程相关接口------------------------------------
 	
+	// 加载关卡
+	bool LoadNewLevel(FName SceneName);
+
+
+private:
+	//-------------------------关卡数据相关------------------------------------
+	//旧的场景名字
+	FName OldSceneName;
+	//新场景名字
+	FName NewSceneName;
+
+private:
+	static USightTourLevelManager* Instance;
 };
