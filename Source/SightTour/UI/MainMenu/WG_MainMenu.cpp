e1964f402759329efaa5f82fb50613f5dd1f5cf2
mnmnsyf <2469073455@qq.com>
2024-05-12 23:47:14 +0800

    1.菜单UI；2.摄像机随机晃动

---
+++ Source/SightTour/UI/MainMenu/WG_MainMenu.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Player/WG_InventoryBar.h"
+#include "Components/ProgressBar.h"
+#include "Pickup/Equipment/EquipmentManagerComponent.h"
+#include "WG_BallSlot.h"
+#include "Components/HorizontalBox.h"
+#include "Engine/Texture2D.h"
+
+
+void UWG_InventoryBar::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	UEquipmentManagerComponent* EquipmentManager = UEquipmentManagerComponent::Get(GetOwningPlayerPawn());
+	if (EquipmentManager)
+	{
+		EquipmentManager->ChangeSlotDelegate.AddDynamic(this, &UWG_InventoryBar::BP_OnBallSlotChange);
+		EquipmentManager->PickupBallDelegate.AddDynamic(this, &UWG_InventoryBar::OnBallPickup);
+	}
+
+	HB_InventoryBox->ClearChildren();
+	for (int32 Index = 0; Index< 5; ++Index)
+	{
+		UWG_BallSlot* BallSlot = LoadBallSlot();
+		if (BallSlot)
+		{
+			BallSlot->SetVisibility(ESlateVisibility::Visible);
+			InventorySlot.Emplace(BallSlot);
+			HB_InventoryBox->AddChildToHorizontalBox(BallSlot);
+		}
+	}
+}
+
+void UWG_InventoryBar::OnBallPickup(bool bPickup, int32 InSlotIndex, FString BallContent, FName BallType)
+{
+	check(InventorySlot.IsValidIndex(InSlotIndex));
+	if (UTexture2D** BallTexture = BallTypeIconMap.Find(BallType))
+	{
+		bPickup ? InventorySlot[InSlotIndex]->SetBallContent(BallContent, *BallTexture)
+			: InventorySlot[InSlotIndex]->ClearBallContent();
+	}
+	else
+	{
+		ensure(false);
+	}
+}
+
+UWG_BallSlot* UWG_InventoryBar::LoadBallSlot()
+{
+	check(!BallSlotClass.IsNull());
+	TSubclassOf<UWG_BallSlot> WidgetClass = BallSlotClass.LoadSynchronous();
+
+	if (ensure(WidgetClass && !WidgetClass->HasAnyClassFlags(CLASS_Abstract)))
+	{
+		UWG_BallSlot* NewObject = CreateWidget<UWG_BallSlot>(GetOwningLocalPlayer()->GetPlayerController(GetWorld()), WidgetClass);
+
+		return NewObject;
+	}
+
+	return nullptr;
+}
---
+++ Source/SightTour/UI/MainMenu/WG_MainMenu.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "WG_InventoryBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIGHTTOUR_API UWG_InventoryBar : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	class UHorizontalBox* HB_InventoryBox;
+
+protected:
+	void NativeConstruct() override;
+
+	UFUNCTION()
+	void OnBallPickup(bool bPickup, int32 InSlotIndex, FString BallContent, FName BallType);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_OnBallSlotChange(int32 LastIndex, int32 NextIndex);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player | HealthBar", meta = (AllowPrivateAccess = "true", AllowAbstract))
+	TSoftClassPtr<class UWG_BallSlot> BallSlotClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player | Ball", meta = (AllowPrivateAccess = "true"))
+	TMap<FName, class UTexture2D*> BallTypeIconMap;
+
+private:
+	UWG_BallSlot* LoadBallSlot();
+
+private:
+	UPROPERTY()
+	TArray<TObjectPtr<class UWG_BallSlot>> InventorySlot;
+};
